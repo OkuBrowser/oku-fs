95e84d78d92794e497cc8c51773f556347215bbe
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2025-02-19 08:50:02 -0500

    feat: Embeddings

--- src/fs/net/embeddings.rs
+++ src/fs/net/embeddings.rs
@@ -1,9 +1,21 @@
 use super::core::{home_replica_filters, EmbeddingModality};
-use crate::fs::OkuFs;
+use crate::{database::posts::core::OkuNote, fs::OkuFs};
+use bytes::Bytes;
+use iroh_blobs::Hash;
 use iroh_docs::DocTicket;
 use miette::IntoDiagnostic;
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use std::{collections::HashMap, path::PathBuf};
+use rayon::iter::IntoParallelIterator;
+use rayon::iter::IntoParallelRefIterator;
+use rayon::iter::ParallelIterator;
+use std::path::PathBuf;
+use url::Url;
+use zebra::{
+    database::default::{
+        audio::DefaultAudioModel, image::DefaultImageModel, text::DefaultTextModel,
+    },
+    model::core::{DatabaseEmbeddingModel, DIM_BGESMALL_EN_1_5, DIM_VIT_BASE_PATCH16_224},
+    Embedding,
+};
 
 impl OkuFs {
     /// The embedding vector database for text media.
@@ -21,6 +33,171 @@ impl OkuFs {
         zebra::database::default::audio::DefaultAudioDatabase::open_or_create(&"audio.zebra".into())
     }
 
+    /// Determine the modality of some data.
+    ///
+    /// # Arguments
+    ///
+    /// * `bytes` - The given data.
+    ///
+    /// # Returns
+    ///
+    /// The modality of the data, if embeddable.
+    pub fn bytes_to_embedding_modality(&self, bytes: &Bytes) -> miette::Result<EmbeddingModality> {
+        let mime_type = tree_magic_mini::from_u8(bytes);
+        let type_ = mime_type.split("/").nth(0).unwrap_or_default();
+        match type_ {
+            "audio" => Ok(EmbeddingModality::Audio),
+            "image" => Ok(EmbeddingModality::Image),
+            "text" => Ok(EmbeddingModality::Text),
+            _ => Err(miette::miette!(
+                "Unexpected MIME type ({mime_type:?}); embedding modality cannot be determined … "
+            )),
+        }
+    }
+
+    /// Create an embedding file in the user's home replica for a document.
+    ///
+    /// # Arguments
+    ///
+    /// * `path` - An optional path to the embedding file; if none is specified, a suggested path will be used.
+    ///
+    /// * `url` - The URL of the document.
+    ///
+    /// * `bytes` - The document's contents.
+    ///
+    /// # Returns
+    ///
+    /// The hash of the file.
+    pub async fn create_post_embedding(
+        &self,
+        path: &Option<PathBuf>,
+        url: &Url,
+        bytes: &Bytes,
+    ) -> miette::Result<Hash> {
+        let home_replica_id = self
+            .home_replica()
+            .await
+            .ok_or(miette::miette!("No home replica set … "))?;
+        let post_path = match path {
+            Some(given_path) => given_path,
+            None => &{
+                let mut path: PathBuf =
+                    OkuNote::suggested_post_path_from_url(&url.to_string()).into();
+                path.set_extension("embed");
+                path
+            },
+        };
+        match self.bytes_to_embedding_modality(bytes)? {
+            EmbeddingModality::Audio => {
+                let model = DefaultAudioModel::default();
+                let embedding = model
+                    .embed(bytes.clone())
+                    .map_err(|e| miette::miette!("{e}"))?;
+                let embedding_json = serde_json::to_string(&embedding).into_diagnostic()?;
+                self.create_or_modify_file(&home_replica_id, post_path, embedding_json)
+                    .await
+            }
+            EmbeddingModality::Image => {
+                let model = DefaultImageModel::default();
+                let embedding = model
+                    .embed(bytes.clone())
+                    .map_err(|e| miette::miette!("{e}"))?;
+                let embedding_json = serde_json::to_string(&embedding).into_diagnostic()?;
+                self.create_or_modify_file(&home_replica_id, post_path, embedding_json)
+                    .await
+            }
+            EmbeddingModality::Text => {
+                let model = DefaultTextModel::default();
+                let embedding = model
+                    .embed(bytes.clone())
+                    .map_err(|e| miette::miette!("{e}"))?;
+                let embedding_json = serde_json::to_string(&embedding).into_diagnostic()?;
+                self.create_or_modify_file(&home_replica_id, post_path, embedding_json)
+                    .await
+            }
+        }
+    }
+
+    /// Find the URLs of the most similar documents.
+    ///
+    /// # Arguments
+    ///
+    /// * `bytes` - A document.
+    ///
+    /// * `number_of_results` - The maximum number of URLs to return.
+    ///
+    /// # Returns
+    ///
+    /// The URLs of the documents most similar to the given one, approximately.
+    pub fn nearest_urls(
+        &self,
+        bytes: &Bytes,
+        number_of_results: usize,
+    ) -> miette::Result<Vec<Url>> {
+        match self.bytes_to_embedding_modality(bytes)? {
+            EmbeddingModality::Audio => {
+                let db = self.audio_database();
+                let results = db
+                    .query_documents(&[bytes.clone()], number_of_results)
+                    .map_err(|e| miette::miette!("{e}"))?;
+                let result_strings: Vec<String> = results
+                    .into_read_only()
+                    .into_par_iter()
+                    .flat_map(|(_x, y)| {
+                        y.into_read_only()
+                            .into_par_iter()
+                            .map(|(_a, b)| String::from_utf8_lossy(&b).to_string())
+                            .collect::<Vec<_>>()
+                    })
+                    .collect();
+                Ok(result_strings
+                    .par_iter()
+                    .filter_map(|x| Url::parse(x).ok())
+                    .collect())
+            }
+            EmbeddingModality::Image => {
+                let db = self.image_database();
+                let results = db
+                    .query_documents(&[bytes.clone()], number_of_results)
+                    .map_err(|e| miette::miette!("{e}"))?;
+                let result_strings: Vec<String> = results
+                    .into_read_only()
+                    .into_par_iter()
+                    .flat_map(|(_x, y)| {
+                        y.into_read_only()
+                            .into_par_iter()
+                            .map(|(_a, b)| String::from_utf8_lossy(&b).to_string())
+                            .collect::<Vec<_>>()
+                    })
+                    .collect();
+                Ok(result_strings
+                    .par_iter()
+                    .filter_map(|x| Url::parse(x).ok())
+                    .collect())
+            }
+            EmbeddingModality::Text => {
+                let db = self.text_database();
+                let results = db
+                    .query_documents(&[bytes.clone()], number_of_results)
+                    .map_err(|e| miette::miette!("{e}"))?;
+                let result_strings: Vec<String> = results
+                    .into_read_only()
+                    .into_par_iter()
+                    .flat_map(|(_x, y)| {
+                        y.into_read_only()
+                            .into_par_iter()
+                            .map(|(_a, b)| String::from_utf8_lossy(&b).to_string())
+                            .collect::<Vec<_>>()
+                    })
+                    .collect();
+                Ok(result_strings
+                    .par_iter()
+                    .filter_map(|x| Url::parse(x).ok())
+                    .collect())
+            }
+        }
+    }
+
     /// Fetch an embedding file associated with a post.
     ///
     /// # Arguments
@@ -30,7 +207,7 @@ impl OkuFs {
     /// * `path` - The path to the file to retrieve.
     ///
     /// * `uri` - The URI associated with the OkuNet post.
-    pub(crate) async fn fetch_post_embeddings(
+    pub async fn fetch_post_embeddings(
         &self,
         ticket: &DocTicket,
         path: &PathBuf,
@@ -40,57 +217,38 @@ impl OkuFs {
             .fetch_file_with_ticket(ticket, path, &Some(home_replica_filters()))
             .await
         {
-            let embeddings = toml::from_str::<HashMap<EmbeddingModality, Vec<f32>>>(
-                String::from_utf8_lossy(&bytes).as_ref(),
-            )
-            .into_diagnostic()?;
-            let text_db = self.text_database();
-            let image_db = self.image_database();
-            let audio_db = self.audio_database();
-            embeddings
-                .into_par_iter()
-                .map(|(modality, embedding)| -> miette::Result<()> {
-                    match modality {
-                        EmbeddingModality::Text => {
-                            text_db
-                                .insert_records(
-                                    &vec![embedding.try_into().unwrap_or_default()],
-                                    &vec![uri.to_owned().into()],
-                                )
-                                .map_err(|e| miette::miette!("{e}"))?;
-                        }
-                        EmbeddingModality::Image => {
-                            image_db
-                                .insert_records(
-                                    &vec![embedding.try_into().unwrap_or_default()],
-                                    &vec![uri.to_owned().into()],
-                                )
-                                .map_err(|e| miette::miette!("{e}"))?;
-                        }
-                        EmbeddingModality::Audio => {
-                            audio_db
-                                .insert_records(
-                                    &vec![embedding.try_into().unwrap_or_default()],
-                                    &vec![uri.to_owned().into()],
-                                )
-                                .map_err(|e| miette::miette!("{e}"))?;
-                        }
-                    }
-                    Ok(())
-                })
-                .collect::<miette::Result<Vec<_>>>()?;
-            text_db
-                .index
-                .deduplicate()
-                .map_err(|e| miette::miette!("{e}"))?;
-            image_db
-                .index
-                .deduplicate()
-                .map_err(|e| miette::miette!("{e}"))?;
-            audio_db
-                .index
-                .deduplicate()
-                .map_err(|e| miette::miette!("{e}"))?;
+            match self.bytes_to_embedding_modality(&bytes)? {
+                EmbeddingModality::Audio => {
+                    let embedding = serde_json::from_str::<Embedding<DIM_VIT_BASE_PATCH16_224>>(
+                        String::from_utf8_lossy(&bytes).as_ref(),
+                    )
+                    .into_diagnostic()?;
+                    let db = self.audio_database();
+                    db.insert_records(&vec![embedding], &vec![uri.to_owned().into()])
+                        .map_err(|e| miette::miette!("{e}"))?;
+                    db.index.deduplicate().map_err(|e| miette::miette!("{e}"))?;
+                }
+                EmbeddingModality::Image => {
+                    let embedding = serde_json::from_str::<Embedding<DIM_VIT_BASE_PATCH16_224>>(
+                        String::from_utf8_lossy(&bytes).as_ref(),
+                    )
+                    .into_diagnostic()?;
+                    let db = self.image_database();
+                    db.insert_records(&vec![embedding], &vec![uri.to_owned().into()])
+                        .map_err(|e| miette::miette!("{e}"))?;
+                    db.index.deduplicate().map_err(|e| miette::miette!("{e}"))?;
+                }
+                EmbeddingModality::Text => {
+                    let embedding = serde_json::from_str::<Embedding<DIM_BGESMALL_EN_1_5>>(
+                        String::from_utf8_lossy(&bytes).as_ref(),
+                    )
+                    .into_diagnostic()?;
+                    let db = self.text_database();
+                    db.insert_records(&vec![embedding], &vec![uri.to_owned().into()])
+                        .map_err(|e| miette::miette!("{e}"))?;
+                    db.index.deduplicate().map_err(|e| miette::miette!("{e}"))?;
+                }
+            }
         }
         Ok(())
     }
